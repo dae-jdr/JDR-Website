f2f42ca98a86c5710b35c7176a79be7ebac48232
KoukiFactori <30896120+DieuKoukiFactori@users.noreply.github.com>
2019-12-05 22:35:18 +0100

    Create typescript project with express and add eslint

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import path from 'path'
+
+import indexRouter from './routes/index'
+
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', indexRouter)
+
+module.exports = app
